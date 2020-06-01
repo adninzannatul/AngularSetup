df4047161f4c329e1813daa827fbbce88ae3f28c
Adnin <adninzannatul1@gmail.com>
2020-06-01 05:38:57 -0400

    Angular Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,10 @@ import { dishdetailComponent } from './Shared/dishdetail.component';
 import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -22,8 +26,11 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     dishdetailComponent,
     HeaderComponent,
-    FooterComponent
-  ],
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
+    ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -32,7 +39,8 @@ import { FooterComponent } from './footer/footer.component';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [
     DishService
